4b0917d79562fea52ae252cfcd5c5cd2d0225a3b
Sithija Shehara <nsithijashehara@gmail.com>
2020-05-01 15:55:28 +0530

    basics

---
+++ lib/Basics.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Basics extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+        title: Text("Fluter Basics",
+        style: TextStyle(color: Color(4278190080))),
+        centerTitle: true,
+        backgroundColor: Colors.lime,
+      ),
+      body: Container(
+        child:Center(
+          child: Text("Sithija"),
+        )
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:weather/Basics.dart';
 import'dart:convert';
 import 'Home.dart';
 
