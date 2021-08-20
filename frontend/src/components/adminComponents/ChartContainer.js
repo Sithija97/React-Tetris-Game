322f0d48edfe3345abfd78c1f2aa5b942f88466d
Sithija Shehara <nsithijashehara@gmail.com>
2021-08-20 18:44:22 +0530

    Added charts with required styling changes

--- frontend/src/components/adminComponents/ChartContainer.js
+++ frontend/src/components/adminComponents/ChartContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MultiAxisLine from "./MultiAxisLine";
 
 function ChartContainer() {
   return (
@@ -12,6 +13,55 @@ function ChartContainer() {
             <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
               <h6 class="m-0 font-weight-bold text-primary">
                 Earnings Overview
+              </h6>
+              {/* <div class="dropdown no-arrow">
+                <a
+                  class="dropdown-toggle"
+                  href="#"
+                  role="button"
+                  id="dropdownMenuLink"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                </a>
+                <div
+                  class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                  aria-labelledby="dropdownMenuLink"
+                >
+                  <div class="dropdown-header">Dropdown Header:</div>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </div>
+              </div> */}
+            </div>
+
+            <div class="card-body">
+              <div class="chart-area">
+                {/* <canvas id="myAreaChart"></canvas> */}
+                <MultiAxisLine />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xl-4 col-lg-5">
+          <div class="card shadow mb-4" style={{ background: "#5f27cd" }}>
+            <div
+              class="card-header py-3 d-flex flex-row align-items-center justify-content-between"
+              style={{ background: "#5f27cd" }}
+            >
+              <h6 class="m-0 font-weight-bold text-primary text-light">
+                Today
               </h6>
               <div class="dropdown no-arrow">
                 <a
@@ -45,70 +95,24 @@ function ChartContainer() {
             </div>
 
             <div class="card-body">
-              <div class="chart-area">
-                <canvas id="myAreaChart"></canvas>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-xl-4 col-lg-5">
-          <div class="card shadow mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-              <div class="dropdown no-arrow">
-                <a
-                  class="dropdown-toggle"
-                  href="#"
-                  role="button"
-                  id="dropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                >
-                  <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div
-                  class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                  aria-labelledby="dropdownMenuLink"
-                >
-                  <div class="dropdown-header">Dropdown Header:</div>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div class="card-body">
               <div class="chart-pie pt-4 pb-2">
-                <canvas id="myPieChart"></canvas>
+                {/* <canvas id="myPieChart"></canvas> */}
               </div>
               <div class="mt-4 text-center small">
                 <span class="mr-2">
-                  <i class="fas fa-circle text-primary"></i> Direct
+                  {/* <i class="fas fa-circle text-primary"></i> Direct */}
                 </span>
                 <span class="mr-2">
-                  <i class="fas fa-circle text-success"></i> Social
+                  {/* <i class="fas fa-circle text-success"></i> Social */}
                 </span>
                 <span class="mr-2">
-                  <i class="fas fa-circle text-info"></i> Referral
+                  {/* <i class="fas fa-circle text-info"></i> Referral */}
                 </span>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-
-
 
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-900 font-weight-bold">WEAK STOCK</h1>
@@ -153,16 +157,96 @@ function ChartContainer() {
 
             <div class="card-body">
               <div class="chart-area">
-                <canvas id="myAreaChart"></canvas>
+                {/* <canvas id="myAreaChart"></canvas> */}
+                <div class="table-responsive">
+                  <table class="table table-striped table-sm">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1,001</td>
+                        <td>Lorem</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                      </tr>
+                      <tr>
+                        <td>1,002</td>
+                        <td>amet</td>
+                        <td>consectetur</td>
+                        <td>adipiscing</td>
+                        <td>elit</td>
+                      </tr>
+                      <tr>
+                        <td>1,003</td>
+                        <td>Integer</td>
+                        <td>nec</td>
+                        <td>odio</td>
+                        <td>Praesent</td>
+                      </tr>
+                      <tr>
+                        <td>1,003</td>
+                        <td>libero</td>
+                        <td>Sed</td>
+                        <td>cursus</td>
+                        <td>ante</td>
+                      </tr>
+                      <tr>
+                        <td>1,004</td>
+                        <td>dapibus</td>
+                        <td>diam</td>
+                        <td>Sed</td>
+                        <td>nisi</td>
+                      </tr>
+
+                      <tr>
+                        <td>1,007</td>
+                        <td>sagittis</td>
+                        <td>ipsum</td>
+                        <td>Praesent</td>
+                        <td>mauris</td>
+                      </tr>
+                      <tr>
+                        <td>1,008</td>
+                        <td>Fusce</td>
+                        <td>nec</td>
+                        <td>tellus</td>
+                        <td>sed</td>
+                      </tr>
+                      <tr>
+                        <td>1,009</td>
+                        <td>augue</td>
+                        <td>semper</td>
+                        <td>porta</td>
+                        <td>Mauris</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
         <div class="col-xl-4 col-lg-5">
-          <div class="card shadow mb-4">
-            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-              <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+          <div
+            class="card shadow mb-4 bg-indigo"
+            style={{ background: "#be2edd" }}
+          >
+            <div
+              class="card-header py-3 d-flex flex-row align-items-center justify-content-between"
+              style={{ background: "#be2edd" }}
+            >
+              <h6 class="m-0 font-weight-bold text-primary text-light">
+                Revenue Sources
+              </h6>
               <div class="dropdown no-arrow">
                 <a
                   class="dropdown-toggle"
@@ -200,13 +284,13 @@ function ChartContainer() {
               </div>
               <div class="mt-4 text-center small">
                 <span class="mr-2">
-                  <i class="fas fa-circle text-primary"></i> Direct
+                  {/* <i class="fas fa-circle text-primary"></i> Direct */}
                 </span>
                 <span class="mr-2">
-                  <i class="fas fa-circle text-success"></i> Social
+                  {/* <i class="fas fa-circle text-success"></i> Social */}
                 </span>
                 <span class="mr-2">
-                  <i class="fas fa-circle text-info"></i> Referral
+                  {/* <i class="fas fa-circle text-info"></i> Referral */}
                 </span>
               </div>
             </div>
