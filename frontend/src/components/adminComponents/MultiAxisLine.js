2c91736d366d14241bceddd02340c0bb59c3bf2a
Sithija Shehara <nsithijashehara@gmail.com>
2021-08-23 10:11:31 +0530

    Made body section centerd

--- frontend/src/components/adminComponents/MultiAxisLine.js
+++ frontend/src/components/adminComponents/MultiAxisLine.js
@@ -1,69 +1,28 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-const DATA_COUNT = 150;
-const labels = [];
-for (let i = 0; i < DATA_COUNT; ++i) {
-  labels.push(i.toString());
-}
-const datapoints = [0, 20, 20, 60, 60, 120, NaN, 180, 120, 125, 105, 110, 170];
 const data = {
-  labels: labels,
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
   datasets: [
     {
-      label: "Cubic interpolation (monotone)",
-      data: datapoints,
-      borderColor: "#be2edd",
-      fill: false,
-      cubicInterpolationMode: "monotone",
-      tension: 0.4,
+      label: "First dataset",
+      data: [33, 53, 85, 41, 44, 65],
+      fill: true,
+      backgroundColor: "rgba(75,192,192,0.2)",
+      borderColor: "rgba(75,192,192,1)",
     },
     {
-      label: "Cubic interpolation",
-      data: datapoints,
-      borderColor: "#5352ed",
-      fill: false,
-      tension: 0.4,
+      label: "Second dataset",
+      data: [33, 25, 35, 51, 54, 76],
+      fill: true,
+      borderColor: "#742774",
     },
   ],
 };
 
-const config = {
-  type: "line",
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      title: {
-        display: true,
-        text: "Chart.js Line Chart - Cubic interpolation mode",
-      },
-    },
-    interaction: {
-      intersect: false,
-    },
-    scales: {
-      x: {
-        display: true,
-        title: {
-          display: true,
-        },
-      },
-      y: {
-        display: true,
-        title: {
-          display: true,
-          text: "Value",
-        },
-        suggestedMin: -10,
-        suggestedMax: 200,
-      },
-    },
-  },
-};
 const MultiAxisLine = () => (
   <>
-    <Line data={data} options={config} />
+    <Line data={data} />
   </>
 );
 
