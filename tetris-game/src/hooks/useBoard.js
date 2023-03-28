949dd1c09cc564dffd030d0725782952e681e6a6
Sithija Shehara <nsithijashehara@gmail.com>
2023-03-28 15:38:21 +0530

    cleaned

--- tetris-game/src/hooks/useBoard.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react"
-import { buildBoard } from "../logic/board";
-
-export const useBoard = ({ rows, columns }) => {
-    const [board, setBoard] = useState(buildBoard({ rows, columns }));
-
-    return [board]
-}
