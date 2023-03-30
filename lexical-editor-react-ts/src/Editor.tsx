def4e1408319e5464bcf80759077ff64b9f31a67
Sithija Shehara <nsithijashehara@gmail.com>
2023-03-30 10:00:29 +0530

    new initial value setting-up approach

--- lexical-editor-react-ts/src/Editor.tsx
+++ lexical-editor-react-ts/src/Editor.tsx
@@ -21,6 +21,7 @@ import ToolbarPlugin from "./plugins/ToolbarPlugin";
 import initialBody from "./json/initialBody.json";
 import initialSubject from "./json/initialSubject.json";
 import TestPlugin from "./plugins/TestPlugin";
+import { $createParagraphNode, $createTextNode, $getRoot } from "lexical";
 
 // import TreeViewPlugin from "./plugins/TreeViewPlugin";
 // import ToolbarPlugin from "./plugins/ToolbarPlugin";
@@ -32,13 +33,35 @@ function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
 }
 
-const EMPTY_CONTENT = JSON.stringify(initialSubject);
+const prepopulatedRichText = () => {
+  const root = $getRoot();
+  const paragraph = $createParagraphNode();
+
+  const text =
+    "If you have answered the survey, disregard this reminder. You are invited to participate in our survey at the following address:[Link to the survey] Password: If you is not automatically logged in to the survey, you can use the following one-time password: [One-time password] Login page: [Link with one-time password]";
+  const parts = text.split(/(\[.*?\])/g);
+
+  parts.forEach((part, i) => {
+    if (part.startsWith("[")) {
+      const node = $createTextNode(part)
+        .toggleFormat("code")
+        .setStyle("color: blue;");
+      paragraph.append(node);
+    } else {
+      const node = $createTextNode(part);
+      paragraph.append(node);
+    }
+  });
+  root.append(paragraph);
+};
+
+// const EMPTY_CONTENT = JSON.stringify(initialSubject);
 
 const editorConfig = {
   namespace: "MyEditor",
   // The editor theme
   theme: ExampleTheme,
-  editorState: EMPTY_CONTENT,
+  editorState: prepopulatedRichText,
   // Handling of errors during update
   onError(error: any) {
     throw error;
